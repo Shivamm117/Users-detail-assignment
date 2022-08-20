e6a11465d0add26b33bc751deb1f3e1984906a51
Shivamm117 <shivam.mahajan117@gmail.com>
2022-08-21 01:04:12 +0530

    gg

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.set('views', 'views');
 app.use('/admin', addRoutes.routes);
 app.use(displayRoutes.routes);
 
-// app.use((req, res, next) => {
-//     res.status(404).render('404', { pageTitle: 'Page Not Found' });
-// });
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
   
 app.listen(4000);
