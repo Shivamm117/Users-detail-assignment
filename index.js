1502e9366698f30493e882ff6a303f62f84cb79d
Shivamm117 <shivam.mahajan117@gmail.com>
2022-08-21 14:06:59 +0530

    gg

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view engine', 'ejs');
+app.set('view engine',  'ejs');
 app.set('views', 'views');
 
 app.use('/admin', addRoutes.routes);
